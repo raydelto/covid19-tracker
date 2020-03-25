30cc5ec9f191ea7859c18773583324eabd7339ea
Raydelto Hernandez [QPS] <raydelto.hernandez@qps.nl>
2020-03-24 23:46:24 -0300

    First functional version

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function getData(){
+    fetch("http://apidashboard.somee.com/api/covid")
+    .then((result)=> result.json())
+    .then((data)=>{
+        let table = document.getElementById("countriesTable");
+        let keys = ['Country', 'TotalCases', 'TotalDeaths', 'TotalRecovered', 'TotCasesx1Mpop'];
+        data.map((entry, i)=>{
+            //This is a workaround for making the sortable library work.
+            //TODO: Make this code more elegant
+            let row = table.insertRow(i + 2);
+            keys.map((key,j)=>{
+                let col = row.insertCell(j);
+                if(j == 0){
+                    col.scope="row";
+                    col.outerHTML = "<th scope=\"row\">" + entry[key] + "</th>";
+                    return;
+                }
+                col.innerHTML = entry[key] === '' ? 0 : entry[key];
+                col.classList.add('text-right');
+
+            });
+        });
+        //This is a workaround for making the sortable library work.
+        //TODO: Make this code more elegant
+        table.deleteRow(1);
+    });
+}
